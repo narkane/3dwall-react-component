2ac26fc4f5669d345d01b625d136965bf8dda315
John Thummel <enikath@gmail.com>
2021-08-02 01:39:45 -0500

    added core App component

---
+++ components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <nav>
+                <h1>Welcome to Boilerplate!</h1>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                </ul>
+            </nav>
+            <main>
+                <p>
+                    This is main page!
+                </p>
+            </main>
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/App';
 
-function App() {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
-}
+import App from "../components/App.js";
 
 ReactDOM.render(
     <App />,
